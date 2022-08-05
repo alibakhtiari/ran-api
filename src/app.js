f46da465b8ee2234a3580995013b2732d06f6c11
alibakhtiari <ali.bakhtiarii@gmail.com>
2022-08-06 01:38:39 +0430

    cleaned starter codes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 require('dotenv').config();
 
 const app = express();
@@ -9,36 +8,31 @@ const cronJob = require('node-cron');
 
 const axios = require('axios');
 
-const sqlite3 = require('sqlite3').verbose();  
+const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('src/ran.db');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({
+    message: 'Hello World!'
+  });
 });
 
 cronJob.schedule('*/1 * * * *', () => {
   console.log('running a task every minutes' + Date.now());
 });
 
-let count = 0;
-
 app.get('/api', (req, res) => {
-db.serialize(function() {  
-    db.all("SELECT * from miner",function(err,rows){  
-        if(err){  
-            console.log(err);  
-        }  
-        else{  
-            res.send(rows);
-        }  
-    });  
-}); 
+  db.serialize(function () {
+    db.all("SELECT * from miner", function (err, rows) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(rows);
+      }
+    });
+  });
 });
 
-app.post('/api', (req, res) => {
-++count;
-res.json({count});
+app.listen(port, () => {
+  console.log(`ran api listening on port ${port}`);
 });
-
-
-app.listen(port, () => {console.log(`ran api listening on port ${port}`);});
